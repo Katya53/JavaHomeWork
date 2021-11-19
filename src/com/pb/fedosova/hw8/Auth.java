a1f0ead1fdc260790206133e4f05cf3d997f8da3
Katya Fedosova <ekaterina.fedosova@privatbank.ua>
2021-11-19 21:33:29 +0200

    dz8 commit

---
+++ src/com/pb/fedosova/hw8/Auth.java
@@ -0,0 +1,35 @@
+package com.pb.fedosova.hw8;
+
+public class Auth {
+    static String login;
+    private static String password;
+
+    public void signUp(String loginNew, String passwordNew, String confirmPasswordNew)
+            throws WrongLoginException,
+            WrongPasswordException {
+
+        String patLogin = "([a-zA-Z0-9]{5,20})";
+        String patPassword = "([a-zA-Z_0-9]{5,})";
+
+        if (loginNew.matches(patLogin)) {
+            login = loginNew;
+        } else {
+            throw new WrongLoginException("Вы ввели неверный логин. Попробуйте заново");
+        }
+
+        if (passwordNew.matches(patPassword) && passwordNew.equals(confirmPasswordNew)) {
+            password = passwordNew;
+            System.out.println("Регистрация успешно завершена!");
+        } else {
+            throw new WrongPasswordException("Ваш пароль не соответствует требованиям. Попробуйте снова");
+        }
+    }
+
+    public void signIn(String loginNew, String passwordNew) throws WrongLoginException {
+        if (loginNew.equals(login) & passwordNew.equals(password)) {
+            System.out.println("Authorization was successful!");
+        } else {
+            throw new WrongLoginException("You entered an incorrect login. Try again");
+        }
+    }
+}
---
+++ src/com/pb/fedosova/hw8/OnlineShop.java
@@ -0,0 +1,44 @@
+package com.pb.fedosova.hw8;
+
+import java.util.Scanner;
+
+public class OnlineShop {
+    public static void main(String[] args) {
+        String login1;
+        String password1;
+        String confirmPassword1;
+        boolean registr = false;
+        Auth auth = new Auth();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Пожалуйста, зарегистрируйтесь.\nВведите логин:");
+        login1 = scanner.next();
+        System.out.println("Введите пароль:");
+        password1 = scanner.next();
+        System.out.println("Введите пароль для подтверждения:");
+        confirmPassword1 = scanner.next();
+        try {
+            auth.signUp(login1, password1, confirmPassword1);
+            registr = true;
+        } catch (WrongLoginException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (WrongPasswordException e2) {
+            System.out.println(e2.getMessage());
+            e2.printStackTrace();
+        }
+        System.out.println("---------------------------");
+        if (registr == true) {
+            System.out.println("Пожалуйста, авторизуйтесь в системе.\nВведите логин:");
+            login1 = scanner.next();
+            System.out.println("Введите пароль:");
+            password1 = scanner.next();
+            try {
+                auth.signIn(login1, password1);
+            } catch (WrongLoginException e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+}
