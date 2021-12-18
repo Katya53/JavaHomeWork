58ed6887c909b6543b5b06be4a72be9a17b189d6
Katya Fedosova <ekaterina.fedosova@privatbank.ua>
2021-12-18 23:32:48 +0200

    dz11 commit

---
+++ src/com/pb/fedosova/hw11/Test.java
@@ -0,0 +1,36 @@
+package com.pb.fedosova.hw11;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.time.LocalDate;
+
+public class Test {
+    public static void main(String[] args) throws JsonProcessingException {
+
+        User user1 = new User("Руслан", LocalDate.of(1989, 01, 10), "+380984657895", "Харьков");
+        User user2 = new User("Алиса", LocalDate.of(1987, 04, 19), "+380634769852", "Днепр");
+        User user3 = new User("Аврора", LocalDate.of(1998, 09, 06), "+380509786143", "Суммы");
+        User user4 = new User("Сергей", LocalDate.of(1988, 12, 11), "+380684120895", "Чернигов");
+        User.showAll();
+
+        System.out.println("Удалить пользователя: ");
+        User.removeUser(user2);
+        User.showAll();
+
+        System.out.println("Добавить пользователя: ");
+        User.getUser(2);
+
+        System.out.println("Сортировать 1-по адресу, 2-по имени: ");
+        User.compare(2);
+        User.showAll();
+
+        System.out.println("Изменить пользователя: ");
+        User.setUser(0, new User("Карина", LocalDate.of(1986, 06, 05), "+380678725874", "Херсон"));
+        User.showAll();
+
+        System.out.println("Записать в файл: ");
+        User.toFile();
+        System.out.println("Запись из файла: ");
+        User.fromFile(User.toFile());
+    }
+}
